e37d04217ec09093e66b7009445c165a1adaa6a9
Andre <andretiburcio@yahoo.com.br>
2024-05-22 13:16:59 -0300

    1st commit

---
+++ getURL.py
@@ -0,0 +1,31 @@
+import requests
+
+
+x = 1
+
+with open('csvfile.csv','w') as file:
+        file.write("num;code;url;status_code;launch_date;launch_vehicle;launch_site;")
+        file.write('\n')
+
+for i in url_spacecraft_id:
+    url = url_base + i
+    r = requests.get(url)
+
+    launch_date_start = r.text.index("Launch Date:")
+    launch_date = r.text[launch_date_start+22:launch_date_start+32:]
+
+    launch_vehicle_start = r.text.index("Vehicle:")
+    launch_vehicle = r.text[launch_vehicle_start+18:launch_vehicle_start+38:]
+
+    launch_site_start = r.text.index("Site:")
+    launch_site = r.text[launch_site_start+15:launch_site_start+50:]
+
+    print(x)
+
+    with open('csvfile.csv','a') as file:
+        file.write(str(x)+';'+i+';'+url+';'+str(r.status_code)+";"+launch_date+";"+launch_vehicle+";"+launch_site+';')
+        file.write('\n') 
+    x=x+1
+
+    #TO DO: INSERIR O NOME DO SATÉLITE
+
